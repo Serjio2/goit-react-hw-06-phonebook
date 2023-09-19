dfaa6ccdc37cef0a39dd30ba3e8160647d033290
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-09-20 01:40:56 +0300

    create store, slice

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,8 +1,9 @@
 import { Formik, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { Component } from 'react';
 import { nanoid } from 'nanoid';
 import { StyledForm, StyledInput } from './ContactForm.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { addNewContact } from 'redux/contactsSlice';
 
 const schema = Yup.object().shape({
   name: Yup.string()
@@ -17,35 +18,40 @@ const initialValues = {
   number: '',
 };
 
-export class ContactForm extends Component {
-  handleSubmitForm = (value, actions) => {
-    this.props.addContact({ ...value, id: nanoid() });
+export const ContactForm = () => {
+  const allcontacts = useSelector(state => state.contacts.items);
+  const dispatch = useDispatch();
+
+  const handleSubmitForm = (value, actions) => {
+    if (allcontacts.find(contact => contact.name === value.name)) {
+      return alert(`${value.name} is already in contacts`);
+    }
+    
+    dispatch(addNewContact({ ...value, id: nanoid() }));
     actions.resetForm();
   };
 
-  render() {
-    return (
-      <Formik
-        initialValues={initialValues}
-        validationSchema={schema}
-        onSubmit={this.handleSubmitForm}
-      >
-        <StyledForm>
-          <label>
-            <p>Name:</p>
-            <StyledInput type="text" name="name" placeholder="Input name" />
-            <ErrorMessage name="name" component="span"/>
-          </label>
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={handleSubmitForm}
+    >
+      <StyledForm>
+        <label>
+          <p>Name:</p>
+          <StyledInput type="text" name="name" placeholder="Input name" />
+          <ErrorMessage name="name" component="span" />
+        </label>
 
-          <label>
-            <p>Number:</p>
-            <StyledInput type="tel" name="number" placeholder="Input number" />
-            <ErrorMessage name="number" component="span"/>
-          </label>
+        <label>
+          <p>Number:</p>
+          <StyledInput type="tel" name="number" placeholder="Input number" />
+          <ErrorMessage name="number" component="span" />
+        </label>
 
-          <button type="submit">Add contact</button>
-        </StyledForm>
-      </Formik>
-    );
-  }
-}
+        <button type="submit">Add contact</button>
+      </StyledForm>
+    </Formik>
+  );
+};
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,12 +1,19 @@
 import { nanoid } from 'nanoid';
 import { StyledContactList } from './ContactList.styled';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'redux/contactsSlice';
 
-export const ContactList = ({ contacts, onDelete }) => {
+export const ContactList = () => {
+
+  const contacts = useSelector(state => state.contacts.items);
+
+  const dispatch = useDispatch();
+
   return (
     <StyledContactList>
       {contacts.map(contact => (
         <li key={nanoid()}>{contact.name}: {contact.number}
-        <button onClick={() => onDelete(contact.id)}>Delete</button>
+        <button onClick={() => dispatch(deleteContact(contact.id))}>Delete</button>
         </li>
       ))}
     </StyledContactList>
