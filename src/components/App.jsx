c64d8053cf9d087d1d07035de65ea8cff4247b8a
Serhii Kondratenko <kondratenko.serhii3@gmail.com>
2023-09-21 01:07:18 +0300

    add persist

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,67 +1,23 @@
 import { GlobalStyle } from './GlobalStyle';
-import { Component } from 'react';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Section } from './Section/Section';
 import { Filter } from './Filter/Filter';
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
+export const App = () => {
+ 
+  return (
+    <div>
+      <GlobalStyle />
 
-  addContact = newName => {
-    if (
-      this.state.contacts.map(contact => contact.name).includes(newName.name)
-    ) {
-      alert(`${newName.name} is already in contacts`);
-    } else {
-      this.setState(prevState => {
-        return {
-          contacts: [...prevState.contacts, newName],
-        };
-      });
-    }
-  };
+      <Section title="Phonebook">
+        <ContactForm  />
+      </Section>
 
-  changeFilter = newFilter => {
-    this.setState({
-      filter: newFilter,
-    });
-  };
-
-  handleDelete = id => {
-    this.setState(prevState => {
-      return {
-        contacts: prevState.contacts.filter(contact => contact.id !== id),
-      };
-    });
-  };
-
-  render() {
-    const { contacts, filter } = this.state;
-
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-
-    return (
-      <div>
-        <GlobalStyle />
-
-        <Section title="Phonebook">
-          <ContactForm addContact={this.addContact} />
-        </Section>
-
-        <Section title="Contacts">
-          <Filter value={this.state.filter} onChange={this.changeFilter} />
-          <ContactList
-            contacts={visibleContacts}
-            onDelete={this.handleDelete}
-          />
-        </Section>
-      </div>
-    );
-  }
-}
+      <Section title="Contacts">
+        <Filter  />
+        <ContactList />
+      </Section>
+    </div>
+  );
+};
